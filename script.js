a4d51bc9071f4b7768d6d188fe51c3af3ec02bd6
Apollonia Sorella <a.sorella@interface3.be>
2020-11-27 10:28:14 +0100

    insertion de la validation

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const questionsReponses = [
 // index pour évitera un méga boucle
 let currentId = 0;
 const btn = document.querySelector(".next");
+const co = document.querySelector("#output");
 // set de fonctions qui gèrent le drag and drop
 // pour les options à drag
 function allowDrop(ev) {
@@ -65,7 +66,7 @@ function createCodeQuestions(arr) {
       answer += `<input type="radio" id='${arr[currentId].id}' name='${arr[currentId].id}' data-index='${arr[currentId].id}' value='${arr[currentId]["reponses"][j]}'><label for='option'>${arr[currentId]["reponses"][j]}</label>`;
     }
   } else if (arr[currentId]["type"] === "reponse-open") {
-    answer += `${arr[currentId].remaining[0]}<input id='input type='text' data-index='${arr[currentId].id}'>${arr[currentId].remaining[1]}`;
+    answer += `${arr[currentId].remaining[0]}<input id='input' type='text' data-index='${arr[currentId].id}'>${arr[currentId].remaining[1]}`;
   } else if (arr[currentId]["type"] === "reponse-drag") {
 
     for (let k = 0; k < arr[currentId]["choices"].length; k++) {
@@ -85,10 +86,72 @@ function createCodeQuestions(arr) {
   questions_container.innerHTML = question;
 }
 
+
+function checkAnswer(level, playerInput=null) // out_consoleOutput won’t work, gonna have to fill the HTML directly here
+{
+    console.log(level);
+    if (playerInput == null) playerInput = document.querySelector("#input").value;
+
+    if (level.type == "reponse-open")
+    {
+        // Assemble the code to send to the interpreter
+        var skulptInput = ""
+        for (i=0; i<level.remaining.length; i++)
+        {
+            if (level.remaining[i] == "")
+                skulptInput += playerInput;
+            else
+                skulptInput += level.remaining[i];
+        }
+
+        let variablesValues = null;
+        if (level.variables != null)
+            variablesValues = JSON.parse(JSON.stringify(level.variables));
+
+        consoleOutput = skulpt(skulptInput, variablesValues);
+                co.innerHTML = consoleOutput;
+
+        // Check if the result is what we expect
+        if (level.reponse != null)
+        {
+            if (playerInput != level.reponse) {
+                console.log("Player input isn’t what was expected");
+                return false;
+            }
+        }
+        if (level.renduconsole != null)
+        {
+            if (consoleOutput != level.renduconsole) {
+                console.log("Console output isn’t what we expected");
+                return false;
+            }
+        }
+        if (level.variables != null)
+        {
+            console.log("Warning: Variables check hasn’t been tested yet");
+            for (i=0; i<variables.length; i++)
+            {
+                if(variablesValues[i].value != level.variables[i].value) {
+                    console.log("Variable " + level.variablesValues[i].name + " isn’t what we expected");
+                    return false;
+                }       
+            }
+        }
+        return true;
+    }
+    else
+    {
+        console.error("checkAnswer() not yet implemented for types other than ’open’");
+    }   
+}
+
 // on gère apd ici ce qu'il se passe au clic du bouton
 btn.addEventListener("click", function () {
-  /* code de validation */
-  goToNext(questionsReponses);
+  console.log(questionsReponses);
+  if(checkAnswer(questionsReponses[currentId]))
+  // séparer le goToNext: le mettre dans le listener d'un autre btn
+  // qui apparait quand le 1er est cliqué
+    goToNext(questionsReponses);
 });
 
 const questions_container = document.querySelector(".questions_container");
