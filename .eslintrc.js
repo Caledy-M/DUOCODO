79eeada556f06f284b7dd95eefdf3ed66c66acc9
Belen Rodriguez <belenrdguez@gmail.com>
2020-11-27 09:58:36 +0100

    creation questionsReponses dans fichier separé

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,15 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'no-var': 'off',
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'vars-on-top': 'off',
+    'func-names': 'off',
+    'prefer-arrow-callback': 'off',
+    'no-restricted-syntax': 'off',
+    'no-plusplus': 'off',
+    'no-loop-func': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
